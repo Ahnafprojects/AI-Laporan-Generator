739d5e54c42a13ab922ad6f2a00be0effb14744d
Muhammad Ahnaf <ahnafabdun2006@gmail.com>
2026-01-26 17:59:47 +0700

    Fix TypeScript error: Remove plan props from UpgradeButton usage

--- app/upgrade/page.tsx
+++ app/upgrade/page.tsx
@@ -125,7 +125,7 @@ export default async function UpgradePage() {
             </div>
             
             <div className="pt-4">
-              <UpgradeButton plan="monthly" />
+              <UpgradeButton />
             </div>
           </CardContent>
         </Card>
@@ -177,7 +177,7 @@ export default async function UpgradePage() {
             </div>
             
             <div className="pt-4">
-              <UpgradeButton plan="yearly" />
+              <UpgradeButton />
             </div>
           </CardContent>
         </Card>
